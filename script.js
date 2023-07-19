ff287d8c425764592ebe1b0e1819a450de3914d1
Ayrus707 <122613157+Ayrus707@users.noreply.github.com>
2023-07-19 17:51:54 +0530

    Netflix-Clone:01

---
+++ script.js
@@ -0,0 +1,28 @@
+// const contentSelect = document.querySelector("#content-select");
+// const contentDivs = document.querySelectorAll(".content-div");
+
+// contentSelect.addEventListener("change", function() {
+//     const selectedValue = contentSelect.value;
+
+//     contentDivs.forEach(div => {
+//         if (div.id === selectedValue) {
+//             div.style.display = "block";
+//         } else {
+//             div.style.display = "none";
+//         }
+//     });
+// });
+
+// // Initially hide all divs except the first one
+// contentDivs.forEach((div, index) => {
+//     if (index === 0) {
+//         div.style.display = "block";
+//     } else {
+//         div.style.display = "none";
+//     }
+// });
+
+
+
+
+
